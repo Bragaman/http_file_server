2bb73076d28ea00edf90a7706589ce9e3fbe5785
Dmitry Bragin <cwbraga70@live.com>
2017-07-12 16:07:41 +0300

    Fix running server

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,14 @@ int main(int argc, char* argv[])
     try
     {
         // Check command line arguments.
-        if (argc != 4)
+        if (argc != 2)
         {
-            std::cerr << "Usage: http_server <address> <port> <doc_root>\n";
-            std::cerr << "  For IPv4, try:\n";
-            std::cerr << "    receiver 0.0.0.0 80 .\n";
-            std::cerr << "  For IPv6, try:\n";
-            std::cerr << "    receiver 0::0 80 .\n";
+            std::cerr << "Usage: http_server on localhost <port>\n";
             return 1;
         }
 
         // Initialise the server.
-        Http::Server s(argv[1], argv[2], argv[3]);
+        Http::Server s("localhost", argv[1], "");
 
         // Run the server until stopped.
         s.run();
